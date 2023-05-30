b80065e91a979f77cd5003e6acdf98598c6526d1
Sofie Swagemakers Herou <sosw22@student.bth.se>
2023-05-30 19:15:56 +0200

    API routes shuffle draw and draw multiple

--- src/Controller/ApiControllerJson.php
+++ src/Controller/ApiControllerJson.php
@@ -5,11 +5,12 @@ namespace App\Controller;
 use App\Card\Card;
 use App\Card\CardDeck;
 use App\Card\CardHand;
-use App\Card\CardGraphic2;
+// use App\Card\CardGraphic2;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ApiControllerJson extends AbstractController
@@ -40,8 +41,8 @@ class ApiControllerJson extends AbstractController
 
     }
 
-    #[Route("/api/deck", name: "deck")]
-    public function jsonNumber(): Response
+    #[Route("/api/deck", name: "api_deck_get", methods: ['GET'])]
+    public function jsonDeck(): Response
     {
         $deck = new CardDeck();
 
@@ -49,7 +50,68 @@ class ApiControllerJson extends AbstractController
             "deck" => $deck->getDeckAsArray(),
         ];
 
+        return new JsonResponse($data);
+    }
+
+    #[Route("/api/deck/shuffle", name: "api_shuffle_post", methods: ['POST'])]
+    public function jsonShuffle(
+        SessionInterface $session
+    ): Response {
+        // Destroy session
+        $_SESSION = [];
+
+        //Start new deck and store in session
+        $shuffleDeck = new CardDeck();
+        $session->set("deck", $shuffleDeck);
+
+        $data = [
+            "deck" => $shuffleDeck->getDeckAsShuffleArray(),
+        ];
 
         return new JsonResponse($data);
+    }
+
+    #[Route("/card/deck/draw", name: "api_deck_draw")]
+    public function jsonDrawCard(
+        SessionInterface $session
+    ): Response {
+        $thisRoundDeck = $session->get("deck");
+
+        $data = [
+        // Draws a card from thisRoundDeck
+        "draw" => $thisRoundDeck->draw(),
+        // Count deck
+        "countDeck" => $thisRoundDeck->countDeck(),
+        // This shows remaining deck
+        "deck" => $thisRoundDeck->getDeckAsShuffleArray(),
+        ];
+
+        return new JsonResponse($data);
+    }
+
+    #[Route("/card/deck/draw/{num<\d+>}", name: "api_draw_number")]
+    public function jsonDrawNumber(
+        int $num,
+        SessionInterface $session
+    ): Response {
+        $thisRoundDeck = $session->get("deck");
+
+        if ($num > $thisRoundDeck->countDeck()) {
+            throw new \Exception("You're out of cards pal!");
+        }
+
+        $handOfCards = [];
+        for ($i = 1; $i <= $num; $i++) {
+            // $thisRoundDeck->draw();
+            $handOfCards[] = $thisRoundDeck->draw();
+        }
+
+        $data = [
+            "handOfCards" => $handOfCards,
+            "countDeck" => $thisRoundDeck->countDeck(),
+            "deck" => $thisRoundDeck->getDeckAsShuffleArray(),
+        ];
+
+        return new jsonResponse($data);
     }
 }
